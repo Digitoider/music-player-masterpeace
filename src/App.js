1739175713b52532babf396c70f1628e8e6a5b4f
Alexander <a.brjakin@avia-centr.ru>
2018-10-30 14:51:09 +0300

    Start to add track list, refs #tracklist.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
 import "./App.css";
 import Player from "./components/player";
+import ControlButton from "./components/controlButton";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="abra card" />
-        <Player />
+        <div className="content-wrapper">
+          <div className="player-track-list">
+            <div className="player-track-list__wrapper">
+              <div className="player-track-list__track">
+                <div className="player-track-list__play-button-wrapper">
+                  <ControlButton
+                    iconClass={
+                      this.props.paused
+                        ? "player-controls__icon-play"
+                        : "player-controls__icon-pause"
+                    }
+                    onClick={() => {
+                      this.performAction();
+                    }}
+                  />
+                </div>
+                <div className="player-track-list__track-info">
+                  <div className="player-track-list__track-title">Title</div>
+                  <div className="player-track-list__track-executor">
+                    Executor
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Player />
+        </div>
       </div>
     );
   }
